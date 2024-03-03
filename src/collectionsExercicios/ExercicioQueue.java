37f2943ba865dd9f0da14a190d57d72c3e198732
vinicius <vinifranco016@gmail.com>
2024-03-03 09:43:02 -0300

    ExercicioQueue

---
+++ src/collectionsExercicios/ExercicioQueue.java
@@ -0,0 +1,62 @@
+package collectionsExercicios;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class ExercicioQueue {
+
+	public static void main(String[] args) {
+		Scanner read = new Scanner(System.in);
+		Scanner nome = new Scanner(System.in);
+		Queue<String> fila = new LinkedList<String>();
+		int op=-1;
+		while(op != 0) {
+
+		System.out.println("*********************************************\n");
+		System.out.println("1 - Adicionar Cliente na Fila");
+		System.out.println("2 - Listar Clientes na Fila");
+		System.out.println("3 - Retirar Cliete da Fila");
+		System.out.println("0 - Sair");
+		System.out.println("\n*********************************************\n");
+		System.out.println("Entre com a opção desejada: ");
+		op = read.nextInt();
+
+		switch (op) {
+
+		case 1:
+			System.out.println("Digite o nome: ");
+			String aux = nome.nextLine();
+			fila.add(aux);
+
+			
+			
+			
+			break;
+
+		case 2:
+			System.out.println("Fila:  " + fila);
+			break;
+
+		case 3:
+
+			if (fila.isEmpty()== true) {
+				System.out.println("Fila vazia!");
+				break;
+			}
+			fila.poll();
+			System.out.println("Fila: " + fila);
+			System.out.println("o cliente foi chamado!");
+			
+			break;
+
+		default: 
+			System.out.println("Programa finalizado!!");
+			break;
+
+		}
+
+	}
+	}
+
+}
